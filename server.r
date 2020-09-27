4c8e8b94bbb7bb24faeca106bbe4f8221f9aa20b
Mojaffar Ansari <mojaffaransari2016@gmail.com>
2020-09-27 14:09:11 +0530

    code added for SAM inside an observEvent

--- server.r
+++ server.r
@@ -33,6 +33,10 @@ if (!require("preprocessCore", character.only=T, quietly=F)) {
   library("preprocessCore")
 }
 
+if(!require("samr",character.only = T)){
+  BiocManager::install("samr")
+  library("samr")
+}
 
 
 
@@ -100,8 +104,12 @@ server <- function(input, output, session) {
     
   })
   #)
+  log2_val1 <- NULL
+  log2_val2 <- NULL
+  # dat1 <- NULL
+  # dat2 <- NULL
   
-  observeEvent(c(input$preprocess,input$normalize), {
+  observeEvent(c(input$preprocess,input$normalize,input$log2), {
     
     
     inFile1 <- input$file1
@@ -115,7 +123,7 @@ server <- function(input, output, session) {
     #dat1<- read.table(inFile$datapath, header=input$header, sep=input$sep, quote=input$quote)
     else{
       
-      dat1<- read.table(inFile1$datapath, header=input$header,  sep=input$sep, quote=input$quote, row.names = 1)
+      dat1 <- read.table(inFile1$datapath, header=input$header,  sep=input$sep, quote=input$quote, row.names = 1)
       dat2 <- read.table(inFile2$datapath, header = input$header, sep = input$sep, quote = input$quote,row.name = 1)
     }
     
@@ -150,8 +158,8 @@ server <- function(input, output, session) {
       res1 = rma(mat1)
       res2 = rma(mat2)
     }
-    log2_val1 <- log2(res1)
-    log2_val2 <- log2(res2)
+    log2_val1 <<- log2(res1)
+    log2_val2 <<- log2(res2)
     #normalizeQuantiles
     output$contents3 <- DT::renderDataTable({
       DT::datatable(res1, class = 'cell-border stripe', options = list(pageLength = 10, scrollY=FALSE, scrollX=TRUE, paging=TRUE)) 
@@ -159,12 +167,72 @@ server <- function(input, output, session) {
     output$contents4 <- DT::renderDataTable({
       DT::datatable(res2, class = 'cell-border stripe', options = list(pageLength = 10, scrollY=FALSE, scrollX=TRUE, paging=TRUE)) 
     })
+    
+    if(input$log2 == T){
     output$log2_value1 <- DT::renderDataTable({
       DT::datatable(log2_val1,class = 'cell-border stripe',options = list(pageLength = 10,scrollY = F,scrollX = T,paging = T))
     })
     output$log2_value2 <- DT::renderDataTable({
       DT::datatable(log2_val2,class = 'cell-border stripe',options = list(pageLength = 10,scrollY = F,scrollX = T,paging = T))
     })
+    }
+    
   })
-}
+observeEvent(input$next1,{
+  output$page1 <- renderUI({
+    fluidPage(
+      titlePanel("Differential Gene expression"),
+      sidebarLayout(
+        sidebarPanel(
+          selectInput("diff","Select a Method",c("SAM","T-test","Edger"),selected = "SAM"),
+          actionButton("process","Process")
+        ),
+        mainPanel()
+      )
+    )
+  })
+})
+observeEvent(c(input$preprocess,input$diff),{
+    
+   # if(input$diff == "SAM"){
+    
+    file_1 <- input$file1
+    file_2 <- input$file2
+    if(is.null(file_1))
+       return(NULL)
+    if(is.null(file_2))
+      return(NULL)
+    else{
+    data1 <- read.table(file_1$datapath, header=T, row.names = 1)
+    data2 <- read.table(file_2$datapath, header=T, row.names = 1)
+    }
+    tab_ctrl_acute <- cbind(data1,data2)
+    log2_ctrl_acute <- cbind(log2_val1,log2_val2)
+    ncl = c(rep(1,ncol(data1)),rep(2,ncol(data2)));
+    sam.data <- list(x=log2_ctrl_acute,y=ncl,geneid=row.names(tab_ctrl_acute),logged2=T,fdr.output=0.05)
+    samr.object <- samr(sam.data,resp.type = "Two class unpaired",nperms=100,random.seed = 12345,testStatistic = "wilcoxon")
+    delta.table <-samr.compute.delta.table(samr.object)
+    View(delta.table)
+    delta <- 0.55
+    siggens.table <- samr.compute.siggenes.table(samr.object,delta,sam.data,delta.table,min.foldchange = 1.5)
+    pvalues <- samr.pvalues.from.perms(samr.object$tt,samr.object$ttstar)
+    ctrl_acute_upgenes <- siggens.table$genes.up
+    ctrl_acute_downgenes <- siggens.table$genes.lo
+
+    write.table(ctrl_acute_upgenes,file="ctrl_acute_upgenes.txt",sep="\t")
+    write.table(ctrl_acute_downgenes,file="ctrl_acute_downgenes.txt",sep="\t")
+
+    ctrl_acute_deg_rowID = sort(as.numeric(rbind(cbind(ctrl_acute_upgenes[,1]),cbind(ctrl_acute_downgenes[,1]))))
+
+    write.table(ctrl_acute_deg_rowID,file = "ctrl_acute_deg_rowID.txt",sep = "\t")
+
+    save.image("E:/NEW HIV conf/work/ctrl_acute.RData")
+    save(ctrl_acute_deg_rowID,file = "E:/NEW HIV conf/ctrl_acute_deg_rowID")
+    save(ctrl_acute_deg_rowID,file = "E:/NEW HIV conf/ctrl_acute_deg_rowID.RData")
+    savehistory("E:/New HIV conf/work/his_ctrl_acute.txt")
+    
+    #} #if finished
+  }
+) #observeEvent finished
+} #function finished
 #)
